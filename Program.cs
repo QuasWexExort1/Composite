c2867e6271a62b7bd5eb1cdab4e0f1d8fa1bc53e
idk <coldzerokot@gmail.com>
2026-09-25 16:23:29 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿public interface IUnit
+{
+    void Attack();
+}
+
+public class Warrior : IUnit
+{
+    private string Name;
+
+    public void Attack()
+    {
+        Console.WriteLine($"Воин {Name} атакует мечом!");
+    }
+}
+
+public class Mage : IUnit
+{
+    private string Name;
+
+    public void Attack()
+    {
+        Console.WriteLine($"Маг {Name} атакует огненным заклинанием!");
+    }
+
+}
+
+public class Squad : IUnit
+{
+    public string Name;
+    private List<IUnit> units = new List<IUnit>();
+    public Squad(string name)
+    {
+        Name = name;
+    }
+    public void Attack()
+    {
+        Console.WriteLine("Второй отряд атакует!");
+        foreach (IUnit unit in units)
+        {
+            unit.Attack();
+        }
+    }
+    public void Add(IUnit unit)
+    {
+        units.Add(unit);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Squad mainsquad = new Squad("Главный отряд");
+        mainsquad.Add(new Warrior());
+        mainsquad.Add(new Mage());
+        
+
+        Squad newsquad = new Squad("Второй отряд");
+        newsquad.Add(new Warrior());
+        newsquad.Add(new Mage());
+    }
+}
